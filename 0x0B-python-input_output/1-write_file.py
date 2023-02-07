a851e3410018e7c0096657836cdb0e73b6b0460f
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2023-02-07 21:35:09 +0100

    This task is all about, read file, numbers, input and outputs fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-# 1-number_of_lines.py
-# Brennan D Baraban <375@holbertonschool.com>
+# 1-write_file.py
 """Defines a text file line-counting function."""
 
 
-def number_of_lines(filename=""):
+def write_file(filename="", text=""):
     """Return the number of lines in a text file."""
     lines = 0
     with open(filename) as f:
