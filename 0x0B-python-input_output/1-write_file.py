578653cc607dba3b75490289cd0a61e405f57144
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2023-02-07 21:57:08 +0100

    This task is all about, read file, numbers, input and outputs fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-# 1-write_file.py
-"""This function writes a string to a text file and returns the number of
-characters written."""
+"""Module: 1-write_file
+This function writes a string to a text file and returns the number of
+characters written
+"""
 
 
 def write_file(filename="", text=""):
-    """This function writes astring to a text file
-    Args:
-    	-filename: the name of the file that is to be write to
+"""This function writes astring to a text file
+Args:
+	-filename: the name of the file that is to be write to
 	-text: the string to be added to the file
-	"""
+"""
 
     with open(filename, 'w+') as f:
-    return f.write(text)
+    	return f.write(text)
