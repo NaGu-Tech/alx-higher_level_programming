741792eba2027435b1006a2fb0ee0ae9e47c800e
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2023-04-28 00:38:47 +0100

    0x11-python-network_1 task done completely fixed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,9 +9,9 @@ from sys import argv
 from urllib import request, parse
 
 if __name__ == "__main__":
-    url = argv[1]
-    email = argv[2]
-    data = parse.urlencode({'email': email})
-    data = data.encode('ascii')
-    with request.urlopen(url, data) as f:
-        print(f.read().decode('utf-8'))
+	url = argv[1]
+	email = argv[2]
+	data = parse.urlencode({'email': email})
+	data = data.encode('ascii')
+with request.urlopen(url, data) as f:
+	print(f.read().decode('utf-8'))
