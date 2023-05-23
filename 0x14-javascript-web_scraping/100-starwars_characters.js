ebc778e08879ce597d1547d67164cb48738c8db6
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2023-05-23 11:33:45 +0000

    tasks for 0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,25 @@
 #!/usr/bin/node
+// prints all characters of a Star Wars movie:
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
+const process = require('process');
+const filmId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+
+request.get(apiUrl, (err, response, body) => {
+  if (err === null) {
+    const film = JSON.parse(body);
+    const characters = film.characters;
     characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
+      request.get(character, (charError, response, body) => {
+        if (err === null) {
+          const characterInfo = JSON.parse(body);
+          console.log(characterInfo.name);
+        } else {
+          console.log(charError);
         }
       });
     });
+  } else {
+    console.log(err);
   }
 });
